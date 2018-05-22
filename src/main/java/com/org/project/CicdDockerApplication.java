e6fd0880735877a079b15a60ad494bb5f27c835d
iannech <INECHENJE@safaricom.net>
2018-05-22 11:18:05 +0300

    springboot project upload

---
+++ src/main/java/com/org/project/CicdDockerApplication.java
@@ -0,0 +1,12 @@
+package com.org.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDockerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/org/project/CicdDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.org.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
